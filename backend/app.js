6c8a6ae39d7a889891dd90313d08dda3511f9283
Usama Hassan <ahmarkhan1233456@gmail.com>
2023-12-29 13:10:49 +0500

    Code comment for furute reference

--- backend/app.js
+++ backend/app.js
@@ -8,3 +8,30 @@ connectDB();
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
+
+/*
+dotenv Configuration:
+
+require("dotenv").config();: This line loads and configures the 'dotenv' package. It allows you to store configuration settings in a file called .env.
+Importing Modules:
+
+const express = require('express');: Imports the 'express' module, which helps in creating a web server and handling HTTP requests.
+Database Connection:
+
+const connectDB = require("./db/db");: Imports a function (connectDB) from a file located in the 'db' folder. This function is likely responsible for connecting to a database.
+Express App Initialization:
+
+const app = express();: Initializes an instance of the Express web application. This 'app' variable is used to configure and run the web server.
+Port Configuration:
+
+const port = process.env.PORT || 8000;: Configures the port number for the server. It uses the value stored in the environment variable 'PORT', and if it's not defined, it defaults to port 8000.
+Database Connection Execution:
+
+connectDB();: Calls the function to connect to the database. This is important before starting the server to ensure a connection is established.
+Server Start:
+
+app.listen(port, () => {...});: Initiates the server to listen for incoming requests on the specified port. When the server starts, it prints a message to the console, indicating the port number it's listening on.
+Console Log:
+
+console.log(Server is listening on port ${port});: Outputs a message to the console, letting you know that the server has successfully started and is listening on a specific port.
+*/
--- backend/db/db.js
+++ backend/db/db.js
@@ -7,3 +7,24 @@ const connectDB = ()=>{
     })
 };
 module.exports = connectDB;
+
+/*
+Importing Mongoose:
+
+const mongoose = require('mongoose');: This line imports the Mongoose library, which helps in interacting with MongoDB (a type of database).
+Defining a Function:
+
+const connectDB = () => {: This declares a function named connectDB.
+Connecting to MongoDB:
+
+mongoose.connect(process.env.MONGO_URL): It attempts to connect to a MongoDB database using the connection URL provided in the environment variable MONGO_URL. This URL typically contains information like the database server location and authentication details.
+Handling Connection Success:
+
+.then((server) => { console.log(DB Connected with the server ${server.connection.host}); }): If the connection is successful, this part is executed. It logs a message indicating that the database is connected and mentions the host of the database server.
+Handling Connection Failure:
+
+.catch((err) => { console.log(error occurred ${err}); }): If there is an error during the connection attempt, this part is executed. It logs an error message, indicating what went wrong.
+Exporting the Function:
+
+module.exports = connectDB;: This makes the connectDB function available for use in other parts of the code. Other files or modules can import and use this function to establish a connection to the MongoDB database.
+*/ 
